453283a0ed1e255ecd8515faa6192537439e717f
Danny <danny.costelloe@gmail.com>
2021-06-01 11:37:20 -0400

    login

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((userInfo) => {
+        localStorage.token = userInfo.token;
+      });
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>Login</label>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Login;
